48758a7453d154159a588b2550b78749bbf24aa7
Emrecan Muslu <emrecan.muslu@gmail.com>
2025-03-11 22:18:30 +0300

    add api client and product service for fetching products

---
+++ src/services/product.service.ts
@@ -0,0 +1,19 @@
+import apiClient from './api';
+import { AxiosResponse } from 'axios';
+import { ProductResponse } from '@/types/api';
+
+class ProductService {
+  getProducts(page: number, limit: number, order: string): Promise<AxiosResponse<ProductResponse>> {
+    const skip = (page - 1) * limit;
+    return apiClient.get('/products', {
+      params: {
+        limit,
+        skip,
+        sortBy: 'price',
+        order
+      }
+    });
+  }
+}
+
+export default new ProductService();
